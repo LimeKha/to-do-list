713b8aa7f5989eaf19a19bed8c1b1964fe6027a2
proget <example@gmail.com>
2025-10-24 02:02:16 +0300

    '5 lesson'

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,11 +1,10 @@
-import { Link } from "react-router-dom";
 import { ToDo } from "../../models/todo-item";
 import classes from "./ListItem.module.scss";
 
 export const ListItem = ({ todo }: { todo: ToDo }) => {
   return (
-    <Link className={`${classes.link} ${todo.isDone ? classes.done : classes.notDone}`} to={`/list/${todo.id}`}>
+    <a className={`${classes.link} ${todo.isDone ? classes.done : classes.notDone}`} target="_blank" rel="noreferrer" href={`/list/${todo.id}`}>
       {todo.text}
-    </Link>
+    </a>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,73 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './assets/scss/normalize.scss';
-import './assets/scss/style.scss'
-import './index.scss';
-import {ToDoListPage} from './pages/ToDoListPage';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { ToDo } from './models/todo-item';
-import { NotFound } from './pages/404';
-import { ItemDescriprion } from './pages/ItemDescriptions';
-import { Layout } from './layouts/Layout';
-
-const todos: ToDo[] = [
-  {
-    id: 0,
-    text: 'First',
-    isDone: false
-  },
-  {
-    id: 1,
-    text: 'Two',
-    isDone: true
-  },
-  {
-    id: 2,
-    text: 'Three',
-    isDone: false
-  },  
-  {
-    id: 3,
-    text: 'Four',
-    isDone: true
-  }
-]
-
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element: <Layout/>,
-    errorElement: <NotFound />,
-    children: [
-      {
-        path: '/',
-        element: <HomePage todos={todos}/>
-      },
-      {
-        path: '/todo',
-        element: <ToDoListPage />
-      },
-      {
-        path: '/list/:id',
-        element: <ItemDescriprion todos={todos}/>
-      }
-    ]
-  },
-  {
-    path:'*',
-    element: <NotFound />
-  }
-], { basename: '/app/'})
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/scss/normalize.scss";
+import "./assets/scss/style.scss";
+import "./index.scss";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
-
